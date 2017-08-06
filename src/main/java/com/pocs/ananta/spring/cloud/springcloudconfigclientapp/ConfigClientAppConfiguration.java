3756981d728ed6511073f294fd099c2e64d6f7a9
Ananta Choudhury <anantac1977@yahoo.com>
2017-08-06 13:33:07 +0530

    Initialized

---
+++ src/main/java/com/pocs/ananta/spring/cloud/springcloudconfigclientapp/ConfigClientAppConfiguration.java
@@ -0,0 +1,20 @@
+package com.pocs.ananta.spring.cloud.springcloudconfigclientapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="some")
+public class ConfigClientAppConfiguration {
+
+	private String property;
+	
+	public String getProperty(){
+		
+		return property;
+	}
+	
+	public void setProperty(String property){
+		this.property = property;
+	}
+}
